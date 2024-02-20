884c58dcd6e8504b016b7ea1022158edf45da434
Alfredo C Navarro Jr <anavarro@hyperglance.com>
2024-02-20 23:48:05 +0800

    Additional documentation

--- src/main/java/com/devstaff/exam/farmcollector/controller/PlantingController.java
+++ src/main/java/com/devstaff/exam/farmcollector/controller/PlantingController.java
@@ -20,21 +20,40 @@ public class PlantingController {
     @Autowired
     PlantingService plantingService;
 
+    /**
+     * Api to create Planting information
+     * @param plantingRequestDTO
+     * @return
+     */
     @PostMapping(value = "/api/v1/planting")
     public ResponseEntity<PlantingResponseDTO> plant(@RequestBody PlantingRequestDTO plantingRequestDTO) {
         return ResponseEntity.ok(plantingService.plant(plantingRequestDTO));
     }
 
+    /**
+     * Api to get harvests for a season.
+     * @param season
+     * @return
+     */
     @GetMapping(value = "/api/v1/harvest-per-season")
     public ResponseEntity<HarvestedResponseDTO> getHarvestedForSeason(@RequestParam String season) {
         return ResponseEntity.ok(plantingService.getHarvestedForSeason(season));
     }
 
+    /**
+     * Api to get harvest for a farm
+     * @param farm
+     * @return
+     */
     @GetMapping(value = "/api/v1/harvest-per-farm")
     public ResponseEntity<HarvestedResponseDTO> getHarvestedForFarm(@RequestParam String farm) {
         return ResponseEntity.ok(plantingService.getHarvestedForFarm(farm));
     }
 
+    /**
+     * Api to get season report.
+     * @return
+     */
     @GetMapping(value = "/api/v1/harvest-season-report")
     public ResponseEntity<HarvestedResponseDTO> getSeasonReport() {
         return ResponseEntity.ok(plantingService.getSeasonReport());
